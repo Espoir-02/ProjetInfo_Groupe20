c57e543550f4912585568267600521e85a7d56d5
marc <id2286@ensai.fr>
2023-10-25 10:48:04 +0200

    squelette non_authentifie

--- source/business_object/utilisateur/non_authentifie.py
+++ source/business_object/utilisateur/non_authentifie.py
@@ -1,1 +1,15 @@
+class Non_Authentifie:
+    def __init__(id):
+        self.id = id
 
+    def creer_compte(authentifiant, mot_de_passe):
+        1
+
+    def connexion(authentifiant, mot_de_passe):
+        1
+
+    def recherche():
+        1
+
+    def historique():
+        1
