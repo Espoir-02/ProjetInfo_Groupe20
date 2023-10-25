a3710d1fd2293b2c78d3bb2c597d1b6c704f2a99
EnzoEnsai <enzo.camboni@eleve.ensai.fr>
2023-10-25 15:49:10 +0200

    enzo 15h49

--- source/business_object/stage_recherche/recherche.py
+++ source/business_object/stage_recherche/recherche.py
@@ -18,3 +18,31 @@ class Recherche:
         self.niveau_experience = niveau_experience
         self.niveau_etude = niveau_etude
         self.salaire_minimum = salaire_minimum
+
+    def changer_salaire(self,i):
+        """
+        Permet de changer le salaire récupéré en format string vers un format utilisable pour le scrapping.
+
+        Parameters
+        ---------
+        i : 
+
+        """
+        
+        if (i == ">= 20 000"):
+            #
+        
+        elif (i == ">= 40 000"):
+            #
+        
+        elif (i == ">= 60 000"):
+            #
+        
+        elif (i == ">= 80 000"):
+            #
+        
+        elif (i == ">= 100 000"):
+            #
+        
+        else:
+            raise ValueError("Le salaire envoyé est incorrect")
