ee2443cb447c63b3f47204f152c9397ffa0c30f4
azertyLOSE <79312013+azertyLOSE@users.noreply.github.com>
2023-10-25 17:09:31 +0200

    Create detail_stage.py essete 17h09

---
+++ source/view/detail_stage.py
@@ -0,0 +1,25 @@
+from stage import *
+
+class detail_stage_view(AbstractView):
+     def __init__(self, stage):
+        self.stage = stage
+
+    def display(self, selection_stage):
+        # Affiche les détails du stage sélectionné
+        stage_details = self.stage.get_stage_details(stage_details)
+
+        # Affiche les détails du Pokémon et propose de retourner à la vue précédente
+        print(f"Intitulé : {stage_details['nom']}")
+        print(f"Durée: {stage_details['durée']}")
+        print(f"Salaire: {stage_details['salaire']}")
+        print(f"Type: {stage_details['type']}")
+        print(f"Entreprise: {stage_details['entreprise']}")
+        questions = [inquirer.List('choice', message='Choisir une option:', choices=['Retour en arrière', 'Quitter'])]
+        answers = inquirer.prompt(questions)
+
+        if answers['choice'] == 'Retour en arrière':
+            # Retourne à la vue précédente
+            return Liste_envie_view.liste_envie_view
+        else:
+            # Termine l'application
+            return 'Exit'
