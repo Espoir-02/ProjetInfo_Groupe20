c7acfb85ae4805f260f5cf609b30859452001ca5
EnzoEnsai <enzo.camboni@eleve.ensai.fr>
2023-10-26 13:09:32 +0200

    enzo 13h09

--- source/view/couche1start/create_compte_view.py
+++ source/view/couche1start/create_compte_view.py
@@ -7,15 +7,36 @@ class CreateCompte_view:
         type = [inquirer.List('choice', message = 'En tant que:', choices=["Eleve","Professeur","Administrateur"])]
         answers_type = inquirer.prompt(type)
 
-        # Demander le nom : à complèter
+        # Demander le nom :
+        nom = [inquirer.List('choice', message = 'Quel est ton nom:', choices=["Eleve","Professeur","Administrateur"])]
+        answers_nom = inquirer.prompt(nom)
 
-        # Demande le prénom : à complèter
+        # Demander le nom en utilisant une question de type 'input':
+        nom = [inquirer.Text('nom', message='Quel est ton nom ?')]
+        answers_nom = inquirer.prompt(nom)
+        # Récupérer la réponse de l'utilisateur:
+        nom_utilisateur = answers_nom['nom']
 
-        # Demander le pseudo : à complèter
+        # Demande le prénom :
+        prenom = [inquirer.Text('prenom', message='Quel est ton prenom ?')]
+        answers_prenom = inquirer.prompt(prenom)
+        # Récupérer la réponse de l'utilisateur:
+        prenom_utilisateur = answers_prenom['prenom']
 
-        # Demander le mot de passe : à complèter
+        # Demander le pseudo :
+        pseudo = [inquirer.Text('pseudo', message='Quel est ton pseudo ?')]
+        answers_pseudo = inquirer.prompt(pseudo)
+        # Récupérer la réponse de l'utilisateur:
+        pseudo_utilisateur = answers_pseudo['pseudo']
+
+        # Demander le mot de passe :
+        mdp = [inquirer.Text('mdp', message='Quel est ton mdp ?')]
+        answers_mdp = inquirer.prompt(mdp)
+        # Récupérer la réponse de l'utilisateur:
+        mdp_utilisateur = answers_mdp['mdp']
 
         # Création de l'identifiant aléatoire :
-        id_authentifie = UtilisateurDAO.create_compte(Utilisateur(type,nom,prenom,pseudo,mdp)) # demander à Marc-Adrien pour relier Utilisateur et DAO (couche métier)
+        id_authentifie = UtilisateurDAO.create_compte(Utilisateur(type,nom_utilisateur,prenom_utilisateur,pseudo_utilisateur,mdp_utilisateur)) 
+                # demander à Marc-Adrien pour relier Utilisateur et DAO (couche métier)
         
         return "Menu_view", id_authentifie
