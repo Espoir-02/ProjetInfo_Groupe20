ef1b45dbb4d26e834d50bcfd76ab318a471742fd
Esete <esete.estifanos@ensai.fr>
2023-10-27 10:57:32 +0200

    essete 10h57

--- source/view/couche2menu/menu_view.py
+++ source/view/couche2menu/menu_view.py
@@ -1,5 +1,7 @@
 from InquirerPy import inquirer
-        
+from recherche_stage_view import Recherche_Stage_View
+from historique_view import HistoriqueView
+from liste_envie_view import Liste_envie_view
 class Menu_view:
     def display(self, type_utilisateur):
         # Affiche le menu principal
@@ -14,15 +16,15 @@ class Menu_view:
 
         if answers['choice'] == 'Rechercher un stage':
             # Redirige vers la vue Stage
-            return 'Recherche_Stage_View'
+            return Recherche_Stage_View().recherche_stage_view()
         
         elif answers['choice'] == 'Accéder à son historique':
             # Redirige vers la vue Historique
-            return 'Historique_View'
+            return HistoriqueView().historique_view()
         
         elif (type_utilisateur in ['professeur', 'eleve', 'administrateur']) and (answers['choice'] == "Accéder à sa liste d'envie"):
             # Redirige vers la vue Envies_View pour les utilisateurs authentifiés
-            return 'Envies_View'
+            return Liste_envie_view().liste_envie_view()
 
         else:
             # Termine l'application
