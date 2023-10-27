5d97d20dd9c18d35bc80d859169c4fae9cd91646
Anysia <votre@email.com>
2023-10-27 16:23:42 +0200

    maj tests

--- source/tests/test_liste_enviedao.py
+++ source/tests/test_liste_enviedao.py
@@ -11,7 +11,9 @@ def test_update_liste_envie():
     # Tester avec un id_utilisateur invalide
     with pytest.raises(TypeError) as exc_info:
         ma_liste_envie.update_liste_envie(id_utilisateur="pas_un_entier", id_stage=42)
-    assert str(exc_info.value) == "l'identifiant de l'utilisateur est un entier numérique"
+    assert (
+        str(exc_info.value) == "l'identifiant de l'utilisateur est un entier numérique"
+    )
 
     # Tester avec un id_stage invalide
     with pytest.raises(TypeError) as exc_info:
@@ -28,7 +30,9 @@ def test_delete_liste_envie():
     # Tester avec un id_utilisateur invalide
     with pytest.raises(TypeError) as exc_info:
         ma_liste_envie.update_liste_envie(id_utilisateur="pas_un_entier", id_stage=42)
-    assert str(exc_info.value) == "l'identifiant de l'utilisateur est un entier numérique"
+    assert (
+        str(exc_info.value) == "l'identifiant de l'utilisateur est un entier numérique"
+    )
 
     # Tester avec un id_stage invalide
     with pytest.raises(TypeError) as exc_info:
@@ -47,4 +51,6 @@ def test_get_liste_envie_by_id():
     # Tester avec un id_utilisateur invalide
     with pytest.raises(TypeError) as exc_info:
         ma_liste_envie.update_liste_envie(id_utilisateur="pas_un_entier")
-    assert str(exc_info.value) == "l'identifiant de l'utilisateur est un entier numérique"
+    assert (
+        str(exc_info.value) == "l'identifiant de l'utilisateur est un entier numérique"
+    )
