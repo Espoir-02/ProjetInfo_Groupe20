0e2826adb803b83324f8dbed3628d14381d81e60
marc <id2286@ensai.fr>
2023-10-27 12:36:57 +0200

    scrapping marc

--- source/business_object/utilisateur/non_authentifie.py
+++ source/business_object/utilisateur/non_authentifie.py
@@ -2,16 +2,3 @@ class Non_Authentifie:
     def __init__(self, id, id_historique):
         self.id = id
         self.historique = id_historique
-
-    def creer_compte(authentifiant, mot_de_passe):
-        1
-
-    def connexion(authentifiant, mot_de_passe):
-        1
-
-    def recherche():
-        1
-
-    def historique():
-        1
-
