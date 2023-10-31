5c5d2687827334ec68b93458b80a890545b1fa52
Anysia <votre@email.com>
2023-10-31 22:35:06 +0100

    listeleve maj finale

--- source/DAO/ListeEleveDAO.py
+++ source/DAO/ListeEleveDAO.py
@@ -3,36 +3,36 @@ from source.exception.exceptions import IdEleveInexistantError
 
 
 class ListeElevesDAO:
-    def update_liste_eleve(self, eleve, id_prof):
+    def update_liste_eleve(self, eleve, id_professeur):
         """Pour ajouter un élève à la liste d'élèves d'un professeur en particulier.
 
         Parameters
         ----------
         eleve: Utilisateur
             L'élève qui va être ajouté
-        id_prof : int
+        id_professeur : int
             L'identifiant du professeur à qui appartient la liste
 
          Examples
         --------
         >>> ma_liste = ListeElevesDAO()  
         >>> eleve = Eleve(nom="Millepertuis", prenom="Iris", id_eleve=6)  
-        >>> id_prof = 456  
-        >>> ma_liste.update_liste_eleve(eleve, id_prof)
+        >>> id_professeur = 456  
+        >>> ma_liste.update_liste_eleve(eleve, id_professeur)
         ----------
         """
-        if not isinstance(id_prof, int):
+        if not isinstance(id_professeur, int):
             raise TypeError("l'identifiant du professeur est un entier numérique")
         with DBConnection().connection as conn:
-            with conn.cursor as cursor:
+            with conn.cursor() as cursor:
                 cursor.execute(
-                    "INSERT INTO liste_eleves(nom, prénom, id_élève, id_prof)"
-                    "VALUES (%(nom)s, %(prénom)s, %(id_eleve)s, %(id_prof)s)",
+                    "INSERT INTO base_projetinfo.liste_eleves(nom, prenom, id_eleve, id_professeur)"
+                    "VALUES (%(nom)s, %(prenom)s, %(id_eleve)s, %(id_professeur)s)",
                     {
                         "nom": eleve.nom,
-                        "prénom": eleve.prenom,
-                        "id_élève": eleve.id_eleve,
-                        "id_prof": id_prof,
+                        "prenom": eleve.prenom,
+                        "id_eleve": eleve.id_eleve,
+                        "id_professeur": id_professeur,
                     },
                 )
 
@@ -48,7 +48,7 @@ class ListeElevesDAO:
 
          Examples
         --------
-        >>> ma_liste = ListeEleveDAO()  # Instanciation de votre DAO
+        >>> ma_liste = ListeElevesDAO()  # Instanciation de votre DAO
         >>> id_eleve = 6
         >>> id_prof = 456 
         >>> ma_liste.delete_liste_eleve(id_eleve, id_prof)
@@ -60,7 +60,7 @@ class ListeElevesDAO:
         with DBConnection().connection as conn:
             with conn.cursor() as cursor:
                 cursor.execute(
-                    "DELETE FROM liste_eleves "
+                    "DELETE FROM base_projetinfo.liste_eleves "
                     "WHERE id_eleve = %(id_eleve)s AND id_professeur=%(id_professeur)s",
                     {"id_eleve": id_eleve, "id_professeur": id_professeur},
                 )
@@ -68,7 +68,7 @@ class ListeElevesDAO:
                     raise IdEleveInexistantError(id_eleve)
 
 
-    def get_liste_eleve_by_id(self, id_prof):
+    def get_liste_eleve_by_id(self, id_professeur):
         """Pour obtenir la liste d'élèves du professeur.
         
         Parameters
@@ -84,22 +84,32 @@ class ListeElevesDAO:
 
         Examples
         --------
-        >>> ma_liste = ListeEleveDAO()  
-        >>> id_prof = 456  
-        >>> liste_eleves = ma_liste.get_liste_eleve_by_id(id_prof)
+        >>> ma_liste = ListeElevesDAO()  
+        >>> id_professeur = 456  
+        >>> liste_eleves = ma_liste.get_liste_eleve_by_id(id_professeur)
         >>> for eleve in liste_eleves:
         ...     print(f"Nom: {eleve['nom']}, Prénom: {eleve['prénom']}")
         """
-        if not isinstance(id_prof, int):
+        if not isinstance(id_professeur, int):
             raise TypeError("l'identifiant du professeur est un entier numérique")
         with DBConnection().connection as conn:
             with conn.cursor() as cursor:
                 cursor.execute(
-                    "SELECT *"
-                    "WHERE id_prof= %(id_prof)s",
-                    {"id_prof": id_prof}
+                    "SELECT * "
+                    " FROM base_projetinfo.liste_eleves "
+                    "WHERE id_professeur= %(id_professeur)s",
+                    {"id_professeur": id_professeur}
                 )
                 liste_eleves = cursor.fetchall()
                 if not liste_eleves:
                     print("La liste d'élèves est vide")
-            return liste_eleves
+                result_list = []
+                for eleve in liste_eleves:
+                    eleve_dict = {
+                        "id_eleve": eleve[0],
+                        "nom": eleve[2],
+                        "prenom": eleve[3],
+                    }
+                    result_list.append(eleve_dict)   
+        return result_list
+
--- source/tests/test_liste_elevedao.py
+++ source/tests/test_liste_elevedao.py
@@ -1,16 +1,16 @@
 import pytest
 from source.DAO.ListeEleveDAO import ListeElevesDAO
-from source.business_object.utilisateur.Utilisateur import Utilisateur
+from source.business_object.utilisateur.eleve import Eleve
 
 
 def test_update_liste_eleve():
     ma_liste_eleve = ListeElevesDAO()
 
-    eleve = Utilisateur(nom="Dupont", prenom="Jean", id_utilisateur=1)
-    id_prof = 85
+    eleve = Eleve(nom="Fleur", prenom="Amaryllis", id_eleve=8)
+    id_professeur = 10
 
     # Test avec des paramètres valides
-    ma_liste_eleve.update_liste_eleve(eleve, id_prof)
+    ma_liste_eleve.update_liste_eleve(eleve, id_professeur)
 
     # Test avec un id_prof invalide
     with pytest.raises(TypeError) as exc_info:
@@ -22,13 +22,13 @@ def test_get_liste_eleve_by_id():
     ma_liste_eleve = ListeElevesDAO()
 
     # Tester avec des paramètres valides
-    liste = ma_liste_eleve.get_liste_eleve_by_id(id_prof=85)
+    liste = ma_liste_eleve.get_liste_eleve_by_id(id_professeur=10)
     assert isinstance(liste, list)
     assert all(isinstance(eleve, dict) for eleve in liste)
 
-    # Tester avec un id_prof invalide
+    # Tester avec un id_professeur invalide
     with pytest.raises(TypeError) as exc_info:
-        ma_liste_eleve.get_liste_eleve_by_id(id_prof="pas_un_entier")
+        ma_liste_eleve.get_liste_eleve_by_id(id_professeur="pas_un_entier")
     assert str(exc_info.value) == "l'identifiant du professeur est un entier numérique"
 
 
@@ -36,14 +36,16 @@ def test_delete_eleve():
     ma_liste_eleve = ListeElevesDAO()
 
     # Tester avec des paramètres valides
-    ma_liste_eleve.delete_eleve(id_eleve=6, id_professeur=85)
+    ma_liste_eleve.delete_eleve(id_eleve=6, id_professeur=10)
 
     # Tester avec un id_eleve invalide
     with pytest.raises(TypeError) as exc_info:
-        ma_liste_eleve.delete_eleve(id_eleve="pas_un_entier", id_professeur=85)
+        ma_liste_eleve.delete_eleve(id_eleve="pas_un_entier", id_professeur=10)
     assert str(exc_info.value) == "l'identifiant de l'élève est un entier numérique"
 
-    # Tester avec un id_prof invalide
+    # Tester avec un id_professeur invalide
     with pytest.raises(TypeError) as exc_info:
-        ma_liste_eleve.delete_eleve(id_eleve=6, id_professeur="pas_un_entier")
+        ma_liste_eleve.delete_eleve(id_eleve=8, id_professeur="pas_un_entier")
     assert str(exc_info.value) == "l'identifiant du professeur est un entier numérique"
+
+
