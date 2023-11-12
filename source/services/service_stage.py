ca79d512c604eafc697e55d02b07b54ea51e7027
Esete <esete.estifanos@ensai.fr>
2023-11-12 13:21:21 +0100

    essete 13h21

--- source/services/service_stage.py
+++ source/services/service_stage.py
@@ -15,14 +15,4 @@ class StageService:
         self.UtilisateurBO = UtilisateurBO
 
     def rechercher_stages_par_titre(self, titre):
-     # Récupérer tous les stages (adaptez cela en fonction de la structure réelle de vos données)
-    tous_les_stages_ids = [stage_id for stage_id in range(1, 1000)]  # Supposons que les stages ont des identifiants de 1 à 1000
-    tous_les_stages = [self.stage_dao.find_stage_by_id(stage_id) for stage_id in tous_les_stages_ids]
-
-    # Filtrer les stages par titre
-    stages_trouves = [stage for stage in tous_les_stages if stage and titre.lower() in stage["titre"].lower()]
-
-    return stages_trouves
-
-
-    
+     
