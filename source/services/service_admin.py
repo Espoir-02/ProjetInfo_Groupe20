3936ae8cbf1d1d6fc2c18ac3f8833662062e5406
Esete <esete.estifanos@ensai.fr>
2023-11-13 20:08:03 +0100

    essete 20h07

---
+++ source/services/service_admin.py
@@ -0,0 +1,4 @@
+class ServiceAdmin:
+    " Créer des méthoes pour supprimer un utilisateur, supprimer un stage "
+
+    
---
+++ source/services/service_suggestion_eleve.py
@@ -0,0 +1,5 @@
+class ServiceSuggestion:
+
+    " Méthode où on stocke dans la liste de suggestion de l'élève les suggestions du prof"
+    " Méthode permettant à un professeur de faire ds propositions aux élèves "
+    " Méthode où le professeur peut consulter la liste de ses élèves"
