e4b4fc6eecf3974192e2cfcc2a6d07518dc4f20e
Anysia <votre@email.com>
2023-11-14 20:24:48 +0100

    critères recherches stagesDAO (lieu, domaine, ent)

--- source/DAO/StageDAO.py
+++ source/DAO/StageDAO.py
@@ -45,7 +45,9 @@ class StageDAO:
                         "lieu": stage.lieu,
                     },
                 )
-                stage.id = cursor.fetchone()[0]  # on récupère l'ID généré à l'aide de cursor.fetchone()["id_stage"]
+                stage.id = cursor.fetchone()[
+                    0
+                ]  # on récupère l'ID généré à l'aide de cursor.fetchone()["id_stage"]
                 # et on l'assigne à stage.id. Cela suppose que notre table a un champ id_stage.
         return stage
 
@@ -109,3 +111,153 @@ class StageDAO:
                 "lieu": stage_bdd[9],
             }
         return stage_dict
+
+    def find_stage_by_lieu(self, lieu):
+        """Pour récupérer les informations des stages qui correspondent à un lieu donné.
+
+        Parameters
+        ---------
+        lieu: str
+            Le lieu qui intéresse l'utilisateur
+
+        Returns
+        -------
+        list of dict
+            Un dictionnaire qui contient toutes les informations des stages correspondants.
+
+        Examples
+        -------
+        >>> db = StageDAO()
+        >>> stage_info = db.find_stage_by_lieu('rennes)
+        >>> print(stage_info)
+        #La liste des stages dont le lieu est 'Rennes'.
+        """
+        if not isinstance(lieu, str):
+            raise TypeError("Le lieu du stage est une chaîne de caractères")
+        with DBConnection().connection as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    "SELECT *                          "
+                    "  FROM base_projetinfo.stage                      "
+                    " WHERE lieu = %(lieu)s",
+                    {"lieu": lieu},
+                )
+                stage_bdd = cursor.fetchall()
+                if not stage_bdd:
+                    print("Aucun stage ne correspond au lieu demandé")
+                result_list = []
+                for stage in stage_bdd:
+                    stage_dict = {
+                        "id-stage": stage_bdd[0],
+                        "titre": stage_bdd[1],
+                        "lien": stage_bdd[2],
+                        "domaine": stage_bdd[3],
+                        "date_publication": stage_bdd[4],
+                        "salaire": stage_bdd[5],
+                        "periode": stage_bdd[6],
+                        "niveau_etudes": stage_bdd[7],
+                        "entreprise": stage_bdd[8],
+                        "lieu": stage_bdd[9],
+                    }
+                    result_list.append(stage_dict)
+        return result_list
+
+    def find_stage_by_domaine(self, domaine):
+        """Pour récupérer les informations des stages qui correspondent à un domaine donné.
+
+        Parameters
+        ---------
+        domaine: str
+            Le domaine qui intéresse l'utilisateur
+
+        Returns
+        -------
+        list of dict
+            Un dictionnaire qui contient toutes les informations des stages correspondants.
+
+        Examples
+        -------
+        >>> db = StageDAO()
+        >>> stage_info = db.find_stage_by_domaine('jardinage')
+        >>> print(stage_info)
+        #La liste des stages dont le domaine est 'jardinage'.
+        """
+        if not isinstance(domaine, str):
+            raise TypeError("Le domaine du stage est une chaîne de caractères")
+        with DBConnection().connection as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    "SELECT *                          "
+                    "  FROM base_projetinfo.stage                      "
+                    " WHERE domaine = %(domaine)s",
+                    {"domaine": domaine},
+                )
+                stage_bdd = cursor.fetchall()
+                if not stage_bdd:
+                    print("Aucun stage ne correspond au domaine demandé")
+                result_list = []
+                for stage in stage_bdd:
+                    stage_dict = {
+                        "id-stage": stage_bdd[0],
+                        "titre": stage_bdd[1],
+                        "lien": stage_bdd[2],
+                        "domaine": stage_bdd[3],
+                        "date_publication": stage_bdd[4],
+                        "salaire": stage_bdd[5],
+                        "periode": stage_bdd[6],
+                        "niveau_etudes": stage_bdd[7],
+                        "entreprise": stage_bdd[8],
+                        "lieu": stage_bdd[9],
+                    }
+                    result_list.append(stage_dict)
+        return result_list
+
+    def find_stage_by_entreprise(self, entreprise):
+        """Pour récupérer les informations des stages qui correspondent à une entreprise donnée.
+
+        Parameters
+        ---------
+        entreprise: str
+            L'entreprise qui intéresse l'utilisateur
+
+        Returns
+        -------
+        list of dict
+            Un dictionnaire qui contient toutes les informations des stages correspondants.
+
+        Examples
+        -------
+        >>> db = StageDAO()
+        >>> stage_info = db.find_stage_by_entreprise('CactusGibus')
+        >>> print(stage_info)
+        #La liste des stages dont l'entreprise est 'CactusGibus'.
+        """
+        if not isinstance(entreprise, str):
+            raise TypeError("L'entreprise du stage est une chaîne de caractères")
+        with DBConnection().connection as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    "SELECT *                          "
+                    "  FROM base_projetinfo.stage                      "
+                    " WHERE entreprise = %(entreprise)s",
+                    {"entreprise": entreprise},
+                )
+                stage_bdd = cursor.fetchall()
+                if not stage_bdd:
+                    print("Aucun stage ne correspond à l'entreprise demandée")
+                result_list = []
+                for stage in stage_bdd:
+                    stage_dict = {
+                        "id-stage": stage_bdd[0],
+                        "titre": stage_bdd[1],
+                        "lien": stage_bdd[2],
+                        "domaine": stage_bdd[3],
+                        "date_publication": stage_bdd[4],
+                        "salaire": stage_bdd[5],
+                        "periode": stage_bdd[6],
+                        "niveau_etudes": stage_bdd[7],
+                        "entreprise": stage_bdd[8],
+                        "lieu": stage_bdd[9],
+                    }
+                    result_list.append(stage_dict)
+        return result_list
