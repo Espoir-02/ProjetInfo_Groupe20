9a429645730ef7914a038d105e83b390e203a3ab
Esete <esete.estifanos@ensai.fr>
2023-11-15 12:39:30 +0100

    essete 12h39

--- source/services/service_liste_envie.py
+++ source/services/service_liste_envie.py
@@ -10,8 +10,8 @@ class ListeEnvieService:
     def supprimer_stage_de_liste_envie(self, id_eleve, id_stage):
         return self.ListeEnvieDAO.delete_liste_envie(id_eleve, id_stage)
 
-    def get_liste_envie_eleve(self, id_eleve):
-        return self.ListeEnvieDAO.get_liste_envie_by_id(id_eleve)
+    def get_liste_envie_eleve(self, id):
+        return self.ListeEnvieDAO.get_liste_envie_by_id(id)
 
     def vider_liste_envie_eleve(self, id_eleve):
         """
--- source/view/Page_option/Liste_envie_view.py
+++ source/view/Page_option/Liste_envie_view.py
@@ -1,20 +1,33 @@
 from source.business_object.listes.voeux import Voeux
 from InquirerPy import inquirer
+from service_liste_envie import ListeEnvieService
+from session_view import Session
+from detail_stage_view import detail_stage_view_eleve
+from detail_stage_view import detail_stage_view_prof
 
 class Liste_envie_view:
-    def __init__(self,id_envie):
-        self.id_envie = id_envie
+    def __init__(self,id):
+        self.id = id
     
     def display(self):
         #Récupère la liste d'envies
-        liste_envie_courant = self.id_historique.get_proposition()
+        service_liste_envie = ListeEnvieService()
+        liste_envie_courant = service_liste_envie.get_liste_envie_eleve(id)
 
         #Affiche la liste d'envies
-        print(liste_envie_courant)
+        for envie in liste_envie_courant:
+            print(f"{envie['titre']} - {envie['domaine']} - {envie['id_stage']}")
 
         #Selectionner un stage en particulier dans la liste d'envies
-        questions = [inquirer.List('selection_stage', message='Sélectionner un stage:', choices=liste_envie_courant)]
+        choix = [envie['id_stage'] for envie in liste_envie_courant] + ["Retour au menu"]
+        questions = [inquirer.List('selection', message='Sélectionner un stage:', choices=choix)]
         answers = inquirer.prompt(questions)
 
-        #Renvoie le pokemon sélectionné
-        return , answers['selection_stage']
+        if answer == "Retour au menu":
+            # Retourner à la vue du menu principal
+            return MenuView()
+        else:
+            if Session().user_type == "eleve":
+                return detail_stage_view_eleve(answers)
+            else:
+                return detail_stage_view_prof(answers)
