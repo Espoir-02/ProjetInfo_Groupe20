9cb9285084dcf714e374c04145d2dc476775d318
Anysia <votre@email.com>
2023-11-16 12:47:22 +0100

    vue connexion fonctionne+modif start_view

--- source/services/service_connexion.py
+++ source/services/service_connexion.py
@@ -1,12 +1,13 @@
 from source.DAO.utilisateur_dao import UtilisateurDAO
+from source.DAO.utilitaire_dao import UtilitaireDAO
 
 
-class UtilisateurService:
+class ConnexionService:
     def __init__(self):
         self.utilisateur_dao = UtilisateurDAO()
 
     def verifier_identifiants(self, pseudo, mot_de_passe):
-        if self.utilisateur_dao.check_pseudo_exists(pseudo):
+        if UtilitaireDAO.check_pseudo_exists(pseudo):
             mot_de_passe_bdd = self.utilisateur_dao.find_mdp(pseudo)
             return mot_de_passe == mot_de_passe_bdd
         return False
--- source/view/Page_principale/start_view.py
+++ source/view/Page_principale/start_view.py
@@ -1,14 +1,15 @@
-import random
+"""import random
 import source.data # supposition pour l'instant, nom à changer selon Espoir
 from source.view.Page_principale.connexion_view import ConnexionView
 from source.view.Page_principale.create_account import CreationCompte_view
 from source.view.Page_principale.mode_invite_view import Mode_invite_view
 from InquirerPy import inquirer
-import inquirer
+
 
 # from source.business_object.utilisateur.  --> il faut importer une méthode vérification de connexion pour un utilisateur quelconque
         # cette méthode est dans la classe DatabaseUtilisateur
         #inquirer.prompt(self.__questions)
+
 
 class Start_view:
     def __init__(self):
@@ -29,4 +30,45 @@ class Start_view:
 
         elif choice == "Continuer en mode invité":
             return Mode_invite_view()
+"""
+
+
+from InquirerPy import prompt
+from source.view.Page_principale.connexion_view import ConnexionView
+from source.view.Page_principale.create_account import CreationCompte_view
+from source.view.Page_principale.mode_invite_view import Mode_invite_view
+
+
+class Start_view:
+    def __init__(self):
+        self.__questions = [
+            {
+                'type': 'checkbox',
+                'name': 'choice',
+                'message': 'Choisissez une option:',
+                'choices': [
+                    "Se connecter",
+                    "S'inscrire",
+                    "Continuer en mode invité"
+                ]
+            }
+        ]
+    
+    def display(self):
+        answers = prompt(self.__questions)
+        choices = answers['choice']
+
+        for choice in choices:
+            if choice == "Se connecter":
+                return ConnexionView()
+            elif choice == "S'inscrire":
+                return CreationCompte_view()
+            elif choice == "Continuer en mode invité":
+                return Mode_invite_view()
+            else:
+                print("Option invalide. Veuillez réessayer.")
+
+    def make_choice(self):
+        return self.display()
+
 
