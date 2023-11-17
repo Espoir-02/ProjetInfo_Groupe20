eaf91caacbfd41c3aee2e90de29a9c73d2e7d18f
Anysia <votre@email.com>
2023-11-17 13:48:45 +0100

    vue liste envie plus ou moins ok

--- source/view/Page_option/Liste_envie_view.py
+++ source/view/Page_option/Liste_envie_view.py
@@ -11,7 +11,7 @@ class Liste_envie_view:
         self.id_eleve = id_eleve
         self.service_liste_envie= ListeEnvieService()
         
-    
+    """
     def display(self):
         #Récupère la liste d'envies
         service_liste_envie = ListeEnvieService()
@@ -37,5 +37,57 @@ class Liste_envie_view:
 
 if __name__ == "__main__":
     id_eleve= Session().user_id
+    liste_envie_view = Liste_envie_view(id_eleve=id_eleve)
+    liste_envie_view.display()
+    """
+
+    def display(self):
+        while True:
+            # Proposer trois choix à l'utilisateur
+            questions = [
+                inquirer.List('menu', message='Que souhaitez-vous faire ?', choices=[
+                    "Consulter la liste d'envies",
+                    "Supprimer un stage de la liste",
+                    "Revenir au menu principal"
+                ])
+            ]
+            user_choice = inquirer.prompt(questions)['menu']
+
+            if user_choice == "Consulter la liste d'envies":
+                # Récupérer et afficher la liste d'envies
+                liste_envie_courant = self.service_liste_envie.get_liste_envie_eleve(self.id_eleve)
+                print(liste_envie_courant)
+                # Demander à l'utilisateur de sélectionner un stage
+                self.select_stage()
+            elif user_choice == "Supprimer un stage de la liste":
+                id_stage = int(input("Entrez l'ID du stage à supprimer : "))
+                self.service_liste_envie.supprimer_stage_de_liste_envie( self.id_eleve, id_stage)
+                print("Stage supprimé avec succès")
+            elif user_choice == "Revenir au menu principal":
+                # Retourner à la vue du menu principal
+                from source.view.Page_option.menu_view import Menu_view
+                menu_view=Menu_view()
+                menu_view.display()
+
+    def select_stage(self):
+        # Récupérer la liste d'envies
+        liste_envie_courant = self.service_liste_envie.get_liste_envie_eleve(self.id_eleve)
+
+        # Sélectionner un stage en particulier dans la liste d'envies
+        choix = [envie['id_stage'] for envie in liste_envie_courant] + ["Retour au menu"]
+        questions = [inquirer.List('selection', message='Sélectionner un stage:', choices=choix)]
+        answers = inquirer.prompt(questions)
+
+        if answers['selection'] == "Retour au menu":
+            # Retourner à la vue du menu principal
+            return MenuView()
+        else:
+            if Session().user_type == "eleve":
+                return detail_stage_view_eleve(answers['selection'])
+            else:
+                return detail_stage_view_prof(answers['selection'])
+
+if __name__ == "__main__":
+    id_eleve = Session().user_id
     liste_envie_view = Liste_envie_view(id_eleve=id_eleve)
     liste_envie_view.display()
