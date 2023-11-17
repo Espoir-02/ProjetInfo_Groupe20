5856550ec53259ccd50763afd3d57f2ef002a997
Anysia <votre@email.com>
2023-11-17 12:18:46 +0100

    dbconnection

--- source/DAO/dbconnection.py
+++ source/DAO/dbconnection.py
@@ -1,19 +1,32 @@
 import psycopg2
 
-
 class DBConnection:
     def __init__(self):
-        self.connection = psycopg2.connect(
-            user="id2225",
-            password="id2225",
-            host="sgbd-eleves.domensai.ecole",
-            port="5432",
-            database="id2225",
-        )
+        try:
+            self.connection = psycopg2.connect(
+                user="id2225",
+                password="id2225",
+                host="sgbd-eleves.domensai.ecole",
+                port="5432",
+                database="id2225",
+            )
+            print("La connexion à la base de données a réussi.")
+        except Exception as e:
+            print(f"Erreur lors de la connexion à la base de données : {e}")
+            # Si une exception se produit, assurez-vous de ne pas essayer de fermer la connexion ici
 
+    def close_connection(self):
+        if hasattr(self, 'connection') and self.connection is not None:
+            self.connection.close()
+            print("La connexion à la base de données a été fermée.")
 
+# Utilisation de la classe DBConnection
 try:
     db = DBConnection()
-    print("La connexion à la base de données a réussi.")
+    # Faites vos opérations de base de données ici
+
+    # Fermez la connexion lorsque vous avez terminé
+    db.close_connection()
+
 except Exception as e:
     print(f"Erreur lors de la connexion à la base de données : {e}")
