508c9a0ae2e6dd145fd3363f137fa7dd7acdef9c
Esete <esete.estifanos@ensai.fr>
2023-11-17 15:21:28 +0100

    essete

--- source/__main__.py
+++ source/__main__.py
@@ -10,12 +10,4 @@ if __name__ == "__main__":
         current_view = current_view.make_choice()    
 
          
-<<<<<<< HEAD
 "current_view = current_view.make_choice() pour passer à la vue suivante mais , pas cohérent avec notre implementation pour linsant"
-
- 
-
- 
-=======
-"current_view = current_view.make_choice() pour passer à la vue suivante mais , pas cohérent avec notre implementation pour linsant"
->>>>>>> f31008d112ff6889549178f7d85975511aeacfe5
