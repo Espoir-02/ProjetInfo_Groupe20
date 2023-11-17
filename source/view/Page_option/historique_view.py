bf8c246b235bb678df5d24a1f6f6e64a3fda2d14
Esete <esete.estifanos@ensai.fr>
2023-11-17 15:56:09 +0100

    essete

--- source/view/Page_option/historique_view.py
+++ source/view/Page_option/historique_view.py
@@ -15,47 +15,48 @@ class HistoriqueView:
 
         if not historique:
             print("L'historique est vide.")
-            from source.view.menu_view import Menu_view
-            return Menu_view()
-
-        option = []
-        for element in historique :
-            stage_service = StageService()
-            stage = stage_service.find_stage_by_id(element["id_stage"])
-            stage_info = f"ID du stage : {element['id_stage']}, Nom du stage : {stage['titre']}, Nom de l'entreprise : {stage['entreprise']}"
-            # Date de consultation : {element['date_consultation']}, 
-            # On rempli la liste d'affichage
-            option.append(inquirer.Option(f"{index}", stage_info)) 
-        
-        #Ajout de l'option pour retourner au menu
-        option.append(inquirer.Option(str(len(historique) + 1), "Retourner au menu"))
-
-        # Affichage de la liste
-        answers = inquirer.prompt([inquirer.List("Stage vu précedemment", "Consulter son historique :", option)])
-        num_selection = int(answers["Stage vu précedemment"])
-        
-        if num_selection == len(historique) + 1:
-            # Retourner au menu principal
-            from source.view.Page_principale.menu_view import Menu_view
+            from source.view.Page_option.menu_view import Menu_view
             return Menu_view()
         
-        element_selection_historique = historique[num_selection - 1]
-    
-        if Session().user_type == "eleve":
-            id_stage = element_selection_historique["id_stage"]
-            from source.view.Page_detail.detail_stage_view import detail_stage_view_eleve
-            detail_view = detail_stage_view_eleve(id_stage)
-            return detail_view
+        else :
+            option = []
+            for element in historique :
+                stage_service = StageService()
+                stage = stage_service.find_stage_by_id(element["id_stage"])
+                stage_info = f"ID du stage : {element['id_stage']}, Nom du stage : {stage['titre']}, Nom de l'entreprise : {stage['entreprise']}"
+                # Date de consultation : {element['date_consultation']}, 
+                # On rempli la liste d'affichage
+                option.append(inquirer.Option(f"{index}", stage_info)) 
+            
+            #Ajout de l'option pour retourner au menu
+            option.append(inquirer.Option(str(len(historique) + 1), "Retourner au menu"))
 
-        elif Session().user_type == "prof":
-            id_stage = element_selection_historique["id_stage"]
-            from source.view.Page_detail.detail_stage_view import detail_stage_view_prof
-            detail_view = detail_stage_view_prof(id_stage)
-            return detail_view
+            # Affichage de la liste
+            answers = inquirer.prompt([inquirer.List("Stage vu précedemment", "Consulter son historique :", option)])
+            num_selection = int(answers["Stage vu précedemment"])
+            
+            if num_selection == len(historique) + 1:
+                # Retourner au menu principal
+                from source.view.Page_option.menu_view import Menu_view
+                return Menu_view()
+            
+            element_selection_historique = historique[num_selection - 1]
         
-        else:
-            detail_view = detail_stage_view_invite["id_stage"]
-            return detail_view
+            if Session().user_type == "eleve":
+                id_stage = element_selection_historique["id_stage"]
+                from source.view.Page_detail.detail_stage_view import detail_stage_view_eleve
+                detail_view = detail_stage_view_eleve(id_stage)
+                return detail_view
+
+            elif Session().user_type == "prof":
+                id_stage = element_selection_historique["id_stage"]
+                from source.view.Page_detail.detail_stage_view import detail_stage_view_prof
+                detail_view = detail_stage_view_prof(id_stage)
+                return detail_view
+            
+            else:
+                detail_view = detail_stage_view_invite["id_stage"]
+                return detail_view
     
     def make_choice(self):
         return self.display()
