8a89a3174ad57af2cc6663e33124648a674cb290
marc <id2286@ensai.fr>
2023-11-23 11:04:16 +0100

    service utilisateur

--- source/view/Page_principale/mode_invite_view.py
+++ source/view/Page_principale/mode_invite_view.py
@@ -1,5 +1,5 @@
 from InquirerPy import inquirer
-from source.services.service_utilisateur import UtilisateurService
+from source.services.service_utilisateur import ServiceUtilisateur
 from source.view.Page_option.menu_view import Menu_view
 from source.view.session_view import Session
 
@@ -11,7 +11,7 @@ class Mode_invite_view:
     def make_choice(self):
         #Pas besoin de demander le type, nom, prénom, pseudo, mdp puisqu'il est anonyme
         #On crée juste l'identifiant aléatoire
-        utilisateur_service = UtilisateurService()  # noqa: E999
+        utilisateur_service = ServiceUtilisateur()  # noqa: E999
         id_non_authentifie = utilisateur_service.creer_compte_anonyme().id # On met rien dedans, juste crée un id aléatoire (unique ?) 
 
         Session().user_id = id_non_authentifie
