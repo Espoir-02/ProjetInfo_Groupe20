d7d61dd92049c38097d1f15c6559d38c2e15f7d1
marc <id2286@ensai.fr>
2023-11-23 11:25:50 +0100

    scrap alpha

--- source/services/scrapping/scrapping_alpha.py
+++ source/services/scrapping/scrapping_alpha.py
@@ -1,14 +1,39 @@
 from source.DAO.StageDAO import StageDAO
-from source.DAO.EntrepriseDAO import EntrepriseDAO
+#from source.DAO.EntrepriseDAO import EntrepriseDAO
 from source.business_object.stage_recherche.stage import Stage
-from source.business_object.stage_recherche.entreprise import Entreprise
+#from source.business_object.stage_recherche.entreprise import Entreprise
+from source.DAO.utilitaire_dao import UtilitaireDAO
+from source.services.service_export import ExporteurStage
+from source.view.session_view import Session
+from source.DAO.HistoriqueDAO import update_historique
+from source.DAO.ListeEnvieDAO import ListeEnvieDAO
 
 from bs4 import BeautifulSoup
 import requests
 
 
 class Scrapping:
-    def scrap(self, url):
+           
+   def display_additional_info(self, stage_info):
+        print(f"{'*' * 40}")
+        print(f"Titre: {stage_info['titre']}")
+        print(f"Entreprise: {stage_info['entreprise']}")
+        print(f"Lieu: {stage_info['lieu']}")
+        print(f"Lien du stage: {stage_info['lien']}")
+        print(f"{'-' * 40}")
+        print(f"Domaine: {stage_info['domaine']}")
+        print(f"Etude: {stage_info['etude']}")
+        print(f"Période: {stage_info['periode']}")
+        print(f"Gratification: {stage_info['gratification']}")
+        print(f"Date de publication: {stage_info['date_publication']}")
+        print(f"{'*' * 40}\n")
+
+
+   def scrap(self, url):
+
+        id_utilisateur=9
+        chemin_fichier_sortie = f'{id_utilisateur}_fichierExport.csv'
+
         response = requests.get(url)
         html = response.text
 
@@ -17,7 +42,8 @@ class Scrapping:
         stages_trouves = soup.find_all("div", {"class": "advert-card-content"})
 
         cpt = 1
-
+        all_stages_info = []
+        liste_id_stages=[]
         for stage in stages_trouves:
             nomstage = stage.find("h2").get_text(strip=True)
             nomentreprise = stage.find("h3").get_text(strip=True)
@@ -35,6 +61,7 @@ class Scrapping:
             periode = nonr
             date_publication = nonr
             gratification = nonr
+
             for i in range(len(tout)):
                 if tout[i].get_text() == 'DOMAINE DE FORMATION':
                     domaine = tout[i+1].get_text()
@@ -50,30 +77,76 @@ class Scrapping:
                     date_publication = phrase.split('- publié le ')[1].strip()
             #etude = tout[i+3].get_text()
             #gratification = tout[i+5].get_text()
-            print(cpt)
-            print("Titre:", nomstage)
-            print("Entreprise:", nomentreprise)
-            print("Lieu:", lieu)
-            print("Domaine:", domaine)
-            print("Etude:", etude)
-            print("Période:", periode)
-            print("Gratification:", gratification)
-            print("Date de publication:", date_publication)
-            print("Lien du stage:", url2)
-            print("\n")
-            cpt = cpt + 1
+            
+            stage_exist=UtilitaireDAO()
+            verification = stage_exist.check_infos_stage_exists(
+                nomstage, url2, domaine, periode, gratification, date_publication, etude, nomentreprise, lieu)
             # Créer un objet Stage
-            nouveau_stage = Stage(titre=nomstage, lien=url2, domaine=domaine,  date_publication=None, periode=periode, salaire=gratification, niveau_etudes=etude, entreprise=nomentreprise, lieu=lieu)
+            if verification==False:
+                nouveau_stage= Stage(titre=nomstage, lien=url2, domaine=domaine,  date_publication=date_publication, periode=periode, salaire=gratification, niveau_etudes=etude, entreprise=nomentreprise, lieu=lieu)
             # Utiliser StageDAO pour créer le stage dans la base de données
-            stage_dao = StageDAO()
-            stage_cree = stage_dao.create_stage(nouveau_stage)
-            #nouv_entreprise=Entreprise(nom_entreprise=nomentreprise, adresse=lieu)
-            #entreprise_DAO=EntrepriseDAO()
-            #entreprise_cree=entreprise_DAO.create_entreprise(nouv_entreprise)
+                stage_cree = StageDAO().create_stage(nouveau_stage)
 
-# le lieu du stage à mettre dans stage directement ou pas
-# changer la longueur des variables
-# prendre en compte l'url
-# il faut relier les stages qu'on vient de chercher à la recherche
-# date de publication a un problème
-# non concordance de certaines variables de la classe stage avec stageDAO
+            # Créez un dictionnaire avec les informations du stage
+            stage_info = {
+                'titre': nomstage,
+                'entreprise': nomentreprise,
+                'lieu': lieu,
+                'lien': url2,
+                'domaine': domaine,
+                'etude': etude,
+                'periode': periode,
+                'gratification': gratification,
+                'date_publication': date_publication
+            }
+
+            all_stages_info.append(stage_info)
+
+            # Affichez les informations de base du stage
+            print(f"{cpt}. {nomstage} - {nomentreprise} - {lieu}")
+            print(f"{'*' * 60}\n")
+
+            # RECUPERATIONS DES ID DES STAGES
+            stage_id = UtilitaireDAO().get_stage_ids(nomstage, url2, domaine, periode, gratification, date_publication, etude, nomentreprise, lieu)
+            # LA LISTE DES ID DES STAGES 
+            liste_id_stages.append(stage_id)
+
+            cpt = cpt + 1
+        
+        choice=0
+        while choice==0:
+            user_choice = input("Veuillez entrer le numéro du stage pour plus d'informations (ou tapez 'q' pour quitter): ")
+            if user_choice.lower() == 'q':
+                break
+
+            try:
+                # Obtenez les informations du stage sélectionné
+                selected_stage_info = all_stages_info[int(user_choice) - 1]
+                 # Affichez les informations supplémentaires
+                self.display_additional_info(selected_stage_info)
+
+            except (ValueError, IndexError):
+                print("Choix invalide. Veuillez entrer un numéro valide.")
+
+            #Ajout du stage à l'historique
+            id_stage_selected=liste_id_stages[int(user_choice) - 1]
+            HistoriqueDAO().update_historique(id_utilisateur,id_stage_selected[0])
+
+            user_choice2 = input("Tapez 1-pour ajouter ce stage à votre liste d'envie\nTapez 2-pour exporter ce stage\nTapez q-pour quitter: ")
+            if user_choice2.lower() == 1:
+                try:
+                    ListeEnvieDAO().update_liste_envie(id_utilisateur,id_stage_selected[0])
+                except (ValueError, IndexError):
+                    print("Choix invalide. Veuillez entrer un numéro valide.")
+            if user_choice2.lower() == 2:
+                try:
+                    ExporteurStage().exporter_donnees(id_utilisateur,id_stage_selected[0],  chemin_fichier_sortie)
+                except (ValueError, IndexError):
+                    print("Choix invalide. Veuillez entrer un numéro valide.")
+            if user_choice2.lower() == 'q':
+                choice=1
+                    
+
+        
+
+            
