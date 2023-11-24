5fab021c52461d7bb182672ca654b89f1a35f72b
Anysia <votre@email.com>
2023-11-24 11:05:22 +0100

    maj pb

--- source/services/scrapping/scrapping.py
+++ source/services/scrapping/scrapping.py
@@ -135,12 +135,12 @@ class Scrapping:
             user_choice2 = input("Tapez 1-pour ajouter ce stage à votre liste d'envie\nTapez 2-pour exporter ce stage\nTapez q-pour quitter: ")
             if user_choice2.lower() == 1:
                 try:
-                    ListeEnvieDAO().update_liste_envie(id_utilisateur,id_stage_selected[0])
+                    ListeEnvieDAO().update_liste_envie(id_utilisateur,id_stage_selected)
                 except (ValueError, IndexError):
                     print("Choix invalide. Veuillez entrer un numéro valide.")
             if user_choice2.lower() == 2:
                 try:
-                    ExporteurStage().exporter_donnees(id_utilisateur,id_stage_selected[0],  chemin_fichier_sortie)
+                    ExporteurStage().exporter_donnees(id_utilisateur,id_stage_selected,  chemin_fichier_sortie)
                 except (ValueError, IndexError):
                     print("Choix invalide. Veuillez entrer un numéro valide.")
             if user_choice2.lower() == 'q':
